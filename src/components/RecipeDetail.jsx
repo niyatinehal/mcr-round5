b06ebd22ff4b21bf839b9efec09176c20e678e7a
Niyati Nehal <niyatinehal58@gmail.com>
2023-07-01 19:59:00 +0530

    updated RecipeDetail.jsx

--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { RecipeContext } from "../context/RecipieContext";
 
 export const RecipeDetail = () => {
+  const { id } = useParams();
+  const { recipeState } = useContext(RecipeContext);
+  const { recipes } = recipeState;
+  const recipe = recipes.find((recipe) => recipe.id === id);
+
+  if (!recipe) {
+    return <div>Recipe not found</div>;
+  }
+
   return (
-    <div>RecipeDetail</div>
-  )
-}
+    <div>
+      <h2>{recipe.name}</h2>
+      <p>Cuisine: {recipe.cuisine}</p>
+      <h3>Ingredients:</h3>
+      <p>{recipe.ingredients}</p>
+      <h3>Instructions:</h3>
+      <p>{recipe.instructions}</p>
+    </div>
+  );
+};
